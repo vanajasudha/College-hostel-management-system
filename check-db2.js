64a86db12999a7adb52331951b033393523e6d52
vanajasudha <2006.vanjasudha@gmail.com>
2026-04-01 10:58:38 +0530

    Removed unnecessary files and updated frontend

--- check-db2.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const db = require("./config/db");
-db.query("DESCRIBE payment", (err, res) => {
-    if (err) console.error("DESCRIBE payment ERRROR:", err);
-    else console.log("PAYMENT SCHEMA:", JSON.stringify(res, null, 2));
-    process.exit();
-});
--- check-db3.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const db = require("./config/db");
-db.query("SELECT * FROM payment LIMIT 1", (err, res) => {
-    if (err) console.error("SELECT ERROR:", err);
-    else console.log("PAYMENT ROW:", JSON.stringify(res, null, 2));
-    process.exit();
-});
