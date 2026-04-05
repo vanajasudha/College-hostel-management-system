85b236d9cdb8a16e70aea4fea3b8643719bea5d0
vanajasudha <2006.vanjasudha@gmail.com>
2026-04-05 10:42:51 +0530

    deleted routes

--- routes/adminRoomRoutes.js
+++
@@ -1,5 +0,0 @@
-// adminRoomRoutes.js
-// Server.js requires this file. Full room management is in adminManageRoutes.js
-const express = require("express");
-const router = express.Router();
-module.exports = router;
--- routes/adminStudentRoutes.js
+++
@@ -1,5 +0,0 @@
-// adminStudentRoutes.js
-// Server.js requires this file. Full student management is in adminManageRoutes.js
-const express = require("express");
-const router = express.Router();
-module.exports = router;
